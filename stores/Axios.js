b1c8c55779649b0e30a70cdd66817265b26d02c7
Shreyas <shreyassk2005@gmail.com>
2023-05-29 07:53:09 +0000

    Checklogin for take-photo Screen added

--- stores/Axios.js
+++ stores/Axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 var Axios = axios.create({
   // baseURL: "http://localhost:9000/api/",
+  // baseURL: "http://192.168.162.147:9000/api/",
   baseURL: "http://192.168.162.147:9000/api/",
   withCredentials: true,
 });
