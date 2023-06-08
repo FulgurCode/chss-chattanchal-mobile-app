9b81b22cd9c72830726ba0800db51ff2bbbdf76d
Shreyas <shreyassk2005@gmail.com>
2023-06-08 15:13:29 +0000

    checklogin debugging

--- stores/CheckLogin.js
+++ stores/CheckLogin.js
@@ -3,32 +3,34 @@ import Axios from "./Axios";
 export function AdminCheckLogin(setIsLoading, router, href) {
   Axios.get("admin/checklogin")
     .then((res) => {
-      setIsLoading(!res.data)
-      if (res.data == false){
-        router(href)
+      console.log(res.data);
+      setIsLoading(!res.data);
+      if (res.data == false) {
+        router(href);
       }
     })
     .catch((err) => {
-      setIsLoading(true)
-      if (err.response==undefined){
+      console.log(err);
+      setIsLoading(true);
+      if (err.response == undefined) {
         setTimeout(() => AdminCheckLogin(setIsLoading, router, href), 5000);
       }
     });
 }
 export function TeacherCheckLogin(setIsLoading, router, href) {
   Axios.get("teacher/checklogin")
-  .then((res) => {
-    // console.log(res.data)
-    setIsLoading(!res.data)
-    if (res.data == false){
-      router(href)
-    }
-  })
-  .catch((err) => {
-    // console.log(err)
-    setIsLoading(true)
-    if (err.response==undefined){
-      setTimeout(() => TeacherCheckLogin(setIsLoading, router, href), 5000);
-    }
-  });
+    .then((res) => {
+      console.log(res.data);
+      setIsLoading(!res.data);
+      if (res.data == false) {
+        router(href);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      setIsLoading(true);
+      if (err.response == undefined) {
+        setTimeout(() => TeacherCheckLogin(setIsLoading, router, href), 5000);
+      }
+    });
 }
