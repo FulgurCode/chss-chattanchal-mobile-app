47fb8a98bbbf58f3f842f8e730bd247b796f0bf5
Shreyas <shreyassk2005@gmail.com>
2023-06-16 09:28:14 +0000

    Bug fix

--- app/teacher/admission/take-photo.js
+++ app/teacher/admission/take-photo.js
@@ -69,7 +69,7 @@ export default function TakePhoto() {
       setFocus(true);
       if (webSocket) {
         setWebSocket(
-          new WebSocket("ws://192.168.162.147:9000/ws/admission-photo")
+          new WebSocket("wss:chattanchalhss.com/ws/admission-photo")
         );
       }
       return () => {
@@ -103,7 +103,7 @@ export default function TakePhoto() {
   useEffect(() => {
     if (appState.current == "active" && focus) {
       setWebSocket(
-        new WebSocket("ws://192.168.162.147:9000/ws/admission-photo")
+        new WebSocket("wss:chattanchalhss.com/ws/admission-photo")
       );
     }
     return () => {};
